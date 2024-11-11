03aaaba9006c78a114f96366e961cb01198b2655
giovanbarreira <giovanbarreira@bol.com.br>
2024-11-11 09:06:24 -0300

    CUnit and static libraries added.

---
+++ apps/staticLib/division_lib.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "division_lib.h"
+// Function to perform division
+double divide(double numerator, double denominator) {
+    if (denominator == 0) {
+        printf("Error: Division by zero is not allowed.\n");
+        return 0;
+    }
+    return numerator / denominator;
+}
