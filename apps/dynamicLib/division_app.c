620c1857782cd74568c78605f56e4657df6ec43b
giovanbarreira <giovanbarreira@bol.com.br>
2024-11-11 23:20:31 -0300

    Dynamic libs added.

---
+++ apps/dynamicLib/division_app.c
@@ -0,0 +1,19 @@
+#include "division_lib.h"
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    result = divide(num1, num2);
+
+    if (num2 != 0) {
+        printf("Result: %.2lf\n", result);
+    }
+
+    return 0;
+}
---
+++ apps/dynamicLib/division_lib.h
@@ -0,0 +1,6 @@
+#ifndef DIVISION_LIB_H
+#define DIVISION_LIB_H
+
+double divide(double numerator, double denominator);
+
+#endif // DIVISION_LIB_H
